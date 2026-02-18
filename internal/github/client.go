86b87bd2a7937535949acf040825f4143e2d8faf
Bart Smykla <bartek@smykla.com>
2026-02-18 08:49:29 +0100

    fix(deps): update go-github from v80/v82 to v83
    Consolidates renovate PRs 290 and 291 into a single upgrade.
    Replaces both v80 and v82 go-github dependencies with v83, updates
    the import path in internal/github/client.go accordingly.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- internal/github/client.go
+++ internal/github/client.go
@@ -11,7 +11,7 @@ import (
 	"time"
 
 	"github.com/cockroachdb/errors"
-	"github.com/google/go-github/v80/github"
+	"github.com/google/go-github/v83/github"
 
 	execpkg "github.com/smykla-skalski/klaudiush/internal/exec"
 )
